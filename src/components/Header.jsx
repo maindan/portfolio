2e88ea469eab806b92f343454de874ed6ef8f056
badb0rn <130112273+badb0rn@users.noreply.github.com>
2023-11-02 12:06:11 -0400

    config rota

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
         <nav className='Header'>
             <h1>//Dan</h1>
             <ul>
-                <li><NavLink to="/portifolio" exact activeClassName="active" >Home</NavLink></li>
-                <li><NavLink to="/portifolio/About" exact activeClassName="active">Sobre</NavLink></li>
-                <li><NavLink to="/portifolio/Contact" exact activeClassName="active">Contato</NavLink></li>
+                <li><NavLink to="/portfolio" exact activeClassName="active" >Home</NavLink></li>
+                <li><NavLink to="/portfolio/About" exact activeClassName="active">Sobre</NavLink></li>
+                <li><NavLink to="/portfolio/Contact" exact activeClassName="active">Contato</NavLink></li>
             </ul>
         </nav>
     )
