ff7c950bda7dd94831cfe1587f9f480a8102032c
badb0rn <130112273+badb0rn@users.noreply.github.com>
2023-11-01 22:41:31 -0400

    correções de config de rota

--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Header />
       <Routes>
         <Route path="/portfolio/" element={<Home />}/>
-        <Route path="/portfolio/" element={<About />}/>
-        <Route path="/portfolio/" element={<Contact />}/>
+        <Route path="/portfolio/About" element={<About />}/>
+        <Route path="/portfolio/Contact" element={<Contact />}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
