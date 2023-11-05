c03e5905feec3ff4d43e33e916cd662e736669cf
badb0rn <130112273+badb0rn@users.noreply.github.com>
2023-11-05 18:47:39 -0400

    serviço email form implementado

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,41 @@
-import '../style/Contact.css'
+import '../style/Contact.css';
+import { useState} from 'react';
+import emailjs from '@emailjs/browser';
+
 
 function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [assunto, setAssunto] = useState('');
+    const [message, setMessage] = useState('');
+    
+    function sendEmail(e) {
+        e.preventDefault();
+
+        if(name === "" && email === "" && message === "") {
+            alert("Preencha todos os campos!");
+            return
+        }
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email
+        }
+
+        emailjs.send("service_ljv1p79", "template_b30369b", templateParams, "9Dn6tHrjsP3qXH7cz")
+        .then((response) => {
+            alert('email enviado!')
+            console.log(response.status, response.text)
+            setName('')
+            setEmail('')
+            setAssunto('')
+            setMessage('')
+        }, (err) => {
+            console.log("Erro: ", err)
+        });
+    };
+
     return(
         <div className="Contact">
             <div className="profile">
@@ -8,14 +43,11 @@ function Contact() {
             </div>
             <div className="form">
                 <h2>//Entre em contato via e-mail!</h2>
-                <form action="https://api.staticforms.xyz/submit" method="post">
-                    <input type="hidden" name="accessKey" value="9b4bc2f0-eebd-4987-adca-724361252914" />
-                    <input type="hidden" name="redirectTo" value="https://maindan.github.io/portfolio/Contact" />
-                    <input type="text" name="name" placeholder="Nome" />
-                    <input type="email" name="email" placeholder="E-mail" />
-                    <input type="text" name="assunto" placeholder="Assunto" />
-                    <textarea name="message" cols="21" rows="10" placeholder="Mensagem" />
-                    <input type="text" name="honeypot" style={{ display: 'none' }} />
+                <form onSubmit={sendEmail}>
+                    <input type="text" name="name" placeholder="Nome" onChange={(e)=>{setName(e.target.value)}} value={name} />
+                    <input type="email" name="email" placeholder="E-mail" onChange={(e)=>{setEmail(e.target.value)}} value={email} />
+                    <input type="text" name="assunto" placeholder="Assunto" onChange={(e)=>{setAssunto(e.target.value)}} value={assunto} />
+                    <textarea name="message" cols="21" rows="10" placeholder="Mensagem" onChange={(e)=>{setMessage(e.target.value)}} value={message} />
                     <input type="submit" name="Submit" />
                 </form>
             </div>
