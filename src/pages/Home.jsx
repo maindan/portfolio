8a5255f14f6c3e4d2bf2a752a1388fda929f616c
badb0rn <130112273+badb0rn@users.noreply.github.com>
2023-11-05 19:02:59 -0400

    ajustes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,12 +16,10 @@ function Home() {
                     <h2 data-aos="fade-down" data-aos-duration='2000'>// Olá!</h2>
                     <p data-aos="fade-zoom-in" data-aos-duration='2000'>Seja bem vindo a minha página! Criei essa Single Page Application para expor meus projetos e concentrar minhas informações pessoais e profissionais.</p>
                 </article>
-                <aside data-aos="fade-zoom-in" data-aos-duration='2000'>
+                <aside data-aos="fade-zoom-in" data-aos-duration='2000' className='projetos'>
                     <h2>Projetos</h2>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolores repellat magni eveniet dicta, excepturi eos maxime ad totam, placeat ex voluptas, quas eius ducimus illo vero est molestias atque!</p>
-                    <div id="projetos">
-                        
-                    </div>
+                    
                 </aside>
             </section>
         </div>
